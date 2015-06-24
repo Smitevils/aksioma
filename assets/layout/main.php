aad3916c77000d66489c42e79812b08a79ffbda4
Alexey <SmiteVils@yandex.ru>
2015-06-25 01:30:48 +0300

    start write key right block

--- assets/layout/main.php
+++ assets/layout/main.php
@@ -1,3 +1,29 @@
 <header class="width-center-block">
-	sefdsf
+	<div class="header-logo-block">
+		<img src="assets/img/logo-1.png" alt="logo">
+		<p>натяжные потолки в <span>Уфе</span> <a href="#">другой город</a></p>
+		<p class="star-micro">от производителя за 1 день</p>
+	</div>
+	<div class="header-certificate-block">
+		<p>В 2012 году нашей<br>компанией сдано:</p>
+		<h4>25737 м2</h4>
+		<p>натяжных потолков</p>
+	</div>
+	<div class="header-phone-block">
+		<div class="header-phone-block-align">
+			<p><span>Телефоны:</span></p>
+			<h6><span>8-800-2000-600</span></h6>
+			<h6><span>8(347)675-36-66</span></h6>
+			<div class="buttont-1">Заказать звонок</div>
+		</div>
+	</div>
+	<div class="clear"></div>
 </header>
+<main class="width-center-block">
+	<div class="key-block">
+		<div class="key-block">
+			<h1>Натяжные потолки</h1>
+			<h2>«под ключ»</h2>
+		</div>
+	</div>
+</main>
