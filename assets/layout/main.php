bc91a81d5b8e3c44a495f8a484e15db34c2b70a7
Alexey <SmiteVils@yandex.ru>
2015-07-03 13:10:03 +0300

    Сверстал Alert

--- assets/layout/main.php
+++ assets/layout/main.php
@@ -19,7 +19,8 @@
 	</div>
 	<div class="clear"></div>
 </header>
-<main class="width-center-block">
+<main>
+<div class="width-center-block">
 	<div class="countdown-block">
 		<div class="countdown-slider"></div>
 		<div class="countdown-bg"></div>
@@ -47,9 +48,9 @@
 		</div>
 		<p>Получите просчет сразу в трех вариантах:</p>
 		<div class="block-stars">
-			<p>Эконом<div class="stars-1"></div></p>
-			<p>Страндарт<div class="stars-3"></div></p>
-			<p>Vip<div class="stars-5"></div></p>
+			<div class="stars-wrap">Эконом<div class="stars-1"></div></div>
+			<div class="stars-wrap">Страндарт<div class="stars-3"></div></div>
+			<div class="stars-wrap">Vip<div class="stars-5"></div></div>
 		</div>
 		<input type="text" placeholder="Имя">
 		<input type="text" placeholder="Телефон">
@@ -128,4 +129,110 @@
 		</div>
 		<!-- === -->
 	</div>
+	<div class="clear"></div>
+	<!-- === -->
+	<div class="padding-block-height-60"></div>
+	<!-- === -->
+	<div class="content-menu">
+		<ul>
+			<li><a href="#" class="active">Отзывы</a></li>
+			<li><a href="#">Цены</a></li>
+			<li><a href="#">Портфолио</a></li>
+			<li><a href="#">Сертификаты</a></li>
+			<li><a href="#">Схема работы</a></li>
+		</ul>
+	</div>
+	<div class="freebie">
+		<div class="freebie-header">
+			<div class="table">
+				<div class="table-cell">
+					<h3>Бесплатный экспресс-замер — что это?</h3>
+				</div>
+			</div>
+		</div>
+		<div class="freebie-content">
+			<!--  -->
+			<div class="freebie-block freebie-block-icon-1">
+				<h6>Просчет сметы в трех вариантах:</h6>
+				<p>
+					— <span>Эконом</span> (потолок по соотношении цена — качество)
+				</p>
+				<p>
+					— <span>Стандарт</span> (полотна от лучших производителей, хиты продаж)
+				</p>
+				<p>
+					— <span>VIP</span> (индивидуальный вариант многоуровневого потолка + фотопечать)
+				</p>
+			</div>
+			<!--  -->
+			<div class="freebie-block freebie-block-icon-2">
+				<h6>Просчет сметы в трех вариантах:</h6>
+				<p>
+					— Как правильно выбрать точечные светильники
+				</p>
+				<p>
+					— Все о правильном расположение светильников
+				</p>
+				<p>
+					— Холодный или теплый свет , что лучше?
+				</p>
+				<p>
+					— Какое количество нужно, чтобы осветить комнату?
+				</p>
+			</div>
+			<!--  -->
+			<div class="freebie-block freebie-block-icon-3">
+				<h6>Рекомендации по выбору цвета и фактуры потолка</h6>
+				<p>
+					— Что лучше глянец или мат
+				</p>
+				<p>
+					— Белые или цветные полотна
+				</p>
+			</div>
+			<input type="text" placeholder="Имя">
+			<input type="text" placeholder="Телефон">
+			<div class="buttont-2">Отправить<br>заявку сейчас</div>
+			<!--  -->
+		</div>
+	</div>
+	<!-- Основной контент сайта -->
+	<div class="content">
+		<!-- Отзывы -->
+		<div class="content-reviews">
+			<div class="reviews-block">
+				
+			</div>
+		</div>
+	</div>
+	<div class="clear"></div>
+</div>
+<div class="fullwidth-block alert-block-bg">
+	<div class="width-center-block alert-block">
+		<h3>4 ключевые проблемы с которыми сталкиваются 95% желающих установить натяжные потолки</h3>
+		<div class="alert-block-text-box">
+			<h6>Продают подделки</h6>
+			<p>Под видом французского дродают дешевый вредный китайский потолок. В итоге вы получаете материал, который годами будет выделять вредные вещества у вас в квартике</p>
+		</div>
+		<div class="alert-block-text-box">
+			<h6>Отсутствие нормального сервиса</h6>
+			<p>Переносят монтаж со дня на день. Обещают одно, куча мусора, вся квартира в пыли и грязи</p>
+		</div>
+		<div class="alert-block-text-box">
+			<h6>Неквалифициро-ванные монтажники</h6>
+			<p>Более 65% предложений на рынке частники-евроремонтники. Не знающие даже базовых правил работы с газовы оборудованием. В результате — многочисленные взрывы в илых домах по всей стране</p>
+		</div>
+		<div class="alert-block-text-box">
+			<h6>Кидают</h6>
+			<p>Делают заманчивое низкое по цене предложение. Берут предоплату и исчезают. Как правило, ни БЭП, ничего уже не поможет. К сожалению, сейчас это повсеместно.</p>
+		</div>
+		<div class="clear"></div>
+		<h4>Не дайте себя обмануть, <a href="#">заказывайте натяжные потолки у нас!</a></h4>
+	</div>
+</div>
+<div class="fullwidth-block seven-reason-bg">
+	<div class="width-center-block seven-reason">
+		<h3>Семь главных причин, почему наши клиенты рекомендуют нас друзьям и знакомым</h3>
+	</div>
+</div>
 </main>
